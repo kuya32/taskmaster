de258418662384d36d9ba24397d030bac3351933
Marchael Acode <m.acode@outlook.com>
2020-10-20 01:50:42 -0700

    Created home, add task, submit confirmation and all tasks pages

---
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -0,0 +1,36 @@
+package com.example.taskmaster;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button addTaskPageButton = AddTask.this.findViewById(R.id.addTaskPageButton);
+        addTaskPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText itemTitleInput = AddTask.this.findViewById(R.id.taskTitle);
+                EditText itemDescriptionInput = AddTask.this.findViewById(R.id.taskDescription);
+                String itemTitle = itemTitleInput.getText().toString();
+                String itemDescription = itemDescriptionInput.getText().toString();
+                System.out.println(String.format("Submitted! New task: %s has been added to the list! Description: %s.", itemTitle, itemDescription));
+
+                Intent goToSubmitConfirmation = new Intent(AddTask.this, SubmitConfirmation.class);
+                goToSubmitConfirmation.putExtra("Title", itemTitle);
+                goToSubmitConfirmation.putExtra("Description", itemDescription);
+                AddTask.this.startActivity(goToSubmitConfirmation);
+            }
+        });
+    }
+}
