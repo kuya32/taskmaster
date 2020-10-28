415c0a597f4c10fe3c1aabb98a2e98621972ff8a
Marchael Acode <m.acode@outlook.com>
2020-10-28 03:02:15 -0700

    Added amplify setup to task master

--- app/src/main/java/com/macode/taskmaster/AddTask.java
+++ app/src/main/java/com/macode/taskmaster/AddTask.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,11 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.room.Room;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
 
 public class AddTask extends AppCompatActivity {
 
@@ -25,6 +31,22 @@ public class AddTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        try {
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.configure(getApplicationContext());
+
+//            // TODO: Ask how to change to shorten task com.amplifyframework.datastore.generated.model.Task
+//            com.amplifyframework.datastore.generated.model.Task task = com.amplifyframework.datastore.generated.model.Task.builder()
+//                    .title("Lab").body("Complete lab")
+//                    .state("assigned").build();
+//
+//            Amplify.API.mutate(ModelMutation.create(task),
+//                    response -> Log.i("Amplify", "Successfully added"),
+//                    error -> Log.e("Amplify", error.toString()));
+        } catch (AmplifyException e) {
+            e.printStackTrace();
+        }
 
         database = Room.databaseBuilder(getApplicationContext(), Database.class, "macode_task_master")
                 .allowMainThreadQueries()
@@ -54,8 +76,8 @@ public class AddTask extends AppCompatActivity {
 
                 notificationManager.notify(89898989, builder.build());
 
-                Task task = new Task(taskTitle, taskDescription, "assigned");
-                database.taskDAO().saveTask(task);
+//                Task task = new Task(taskTitle, taskDescription, "assigned");
+//                database.taskDAO().saveTask(task);
 
                 Toast toast = Toast.makeText(AddTask.this, "You added a new task", Toast.LENGTH_LONG);
                 toast.show();
